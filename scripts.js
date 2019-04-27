0519fa8dccf2e6d585d84461bb7ec295d124e6b1
Timofey Timko <t.t.home@live.ru>
2019-04-27 11:27:40 +0700

    small code refactoring

--- scripts.js
+++ scripts.js
@@ -1,13 +1,56 @@
-'use strict';
+function init() {
+	const menuElem = document.querySelector('.menu');
+	const navButtonElem = document.querySelector('.navButton');
 
-let myMenu = document.getElementById('menu');
+	window.onscroll = function () {
+		const menuCoords = menuElem.offsetHeight + 100;
 
-myMenu.onclick = function menuShow() {
-	let navMenu = document.getElementById('myTopNav');
+		if (menuCoords < pageYOffset) {
+			navButtonElem.style.opacity = '1';
+			navButtonElem.style.cursor = 'pointer';
+		} else {
+			navButtonElem.style.opacity = '0';
+			navButtonElem.style.cursor = 'auto';
+		}
+	};
 
-	if (navMenu.classList.length === 1) {
-		navMenu.classList.add('responsive');
-	} else {
-		navMenu.classList.remove('responsive');
+	navButtonElem.onclick = function () {
+		if (this.style.opacity === '0') return false;
+		scrollToElement(menuElem.getBoundingClientRect().top);
+	};
+
+	function scrollToElement(topCoord) {
+		window.scrollBy({
+			top: topCoord,
+			left: 0,
+			behavior: 'smooth'
+		});
 	}
-};
+
+	function getAge() {
+		const ageElem = document.querySelector('.description p:last-child');
+
+		const date = new Date();
+		const years = date.getFullYear() - 1993;
+		const day = date.getDate() - 4;
+
+		ageElem.innerHTML = `${years} лет, ${day} дней`;
+	}
+
+	getAge();
+
+	/*const myMenu = document.getElementById('menu');
+
+	myMenu.onclick = function menuShow() {
+		let navMenu = document.getElementById('myTopNav');
+
+		if (navMenu.classList.length === 1) {
+			navMenu.classList.add('responsive');
+		} else {
+			navMenu.classList.remove('responsive');
+		}
+	};*/
+}
+
+document.addEventListener("DOMContentLoaded", init);
+
